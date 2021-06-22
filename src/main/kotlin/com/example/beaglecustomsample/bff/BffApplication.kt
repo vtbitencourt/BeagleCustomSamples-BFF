ba525fbc825e8068ffde305be69b85dffec8791f
Victor Bitencourt <victor.bitencourt@vhbsolution.com.br>
2021-06-21 23:27:49 -0300

    initial beagle config

---
+++ src/main/kotlin/com/example/beaglecustomsample/bff/BffApplication.kt
@@ -0,0 +1,11 @@
+package com.example.beaglecustomsample.bff
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BffApplication
+
+fun main(args: Array<String>) {
+	runApplication<BffApplication>(*args)
+}
